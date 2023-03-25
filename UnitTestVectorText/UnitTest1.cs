0c7214e7e6e8ab645a391aad0f4bb06c4ac74930
OshchepkobDmitiy <odo.2003@yandex.ru>
2023-03-25 15:06:51 +0300

    ok

--- UnitTestVectorText/UnitTest1.cs
+++ UnitTestVectorText/UnitTest1.cs
@@ -2,11 +2,13 @@
 using System;
 using System.IO;
 using System.Linq;
+using DocumentLib;
+using operation;
 
 namespace VectorText
 {
     [TestClass]
-    public class UnitTest
+    public class UnitTestDocument
     {
         [TestMethod]
         public void ReadFromFile()
@@ -31,18 +33,23 @@ namespace VectorText
                     " привлечь актеров, которые смогли бы сыграть его" +
                     " произведения, — так появился театр. ");
             }
-            VectorText vectorText = new VectorText(path, TextOrPath.Path);
-            double norm = VectorText.NormVector(vectorText.vector);
+            Document doc = new Document(path, TextOrPath.Path);
+            double norm = Document.NormVector(doc.vector);
             Assert.IsTrue(Math.Abs(norm - 1) < Math.Pow(10, -8));
         }
 
         [TestMethod]
         public void ReadString()
         {
-            VectorText v1 = new VectorText("a, b, c, c");
-            double norm = VectorText.NormVector(v1.vector);
+            Document v1 = new Document("a, b, c, c");
+            double norm = Document.NormVector(v1.vector);
             Assert.IsTrue(Math.Abs(norm - 1) < Math.Pow(10, -8));
         }
+    }
+
+    [TestClass]
+    public class UnitTestOperation
+    {
 
         [TestMethod]
         public void CosCollen()
@@ -52,61 +59,61 @@ namespace VectorText
             {
                 sw.WriteLine("a b c");
             }
-            VectorText v1 = new VectorText(path);
-            VectorText v2 = new VectorText(path);
-            Assert.IsTrue(Math.Abs(VectorText.Cos(v1, v2) - 1) < Math.Pow(10, -8));
+            Document v1 = new Document(path, TextOrPath.Path);
+            Document v2 = new Document(path, TextOrPath.Path);
+            Assert.IsTrue(Math.Abs(Operation.Cos(v1, v2) - 1) < Math.Pow(10, -8));
         }
 
         [TestMethod]
         public void RightAngel()
         {
-            VectorText v1 = new VectorText("b");
-            VectorText v2 = new VectorText("a");
-            Assert.IsTrue(Math.Abs(VectorText.Cos(v1, v2)) < Math.Pow(10, -8));
+            Document v1 = new Document("b");
+            Document v2 = new Document("a");
+            Assert.IsTrue(Math.Abs(Operation.Cos(v1, v2)) < Math.Pow(10, -8));
         }
 
         [TestMethod]
         public void ZeroVector()
         {
-            VectorText v1 = new VectorText("");
-            VectorText v2 = new VectorText("aaa");
-            Assert.IsTrue(Math.Abs(VectorText.Cos(v1, v2)) < Math.Pow(10, -8));
+            Document v1 = new Document("");
+            Document v2 = new Document("aaa");
+            Assert.IsTrue(Math.Abs(Operation.Cos(v1, v2)) < Math.Pow(10, -8));
         }
 
         [TestMethod]
         public void Cos()
         {
-            VectorText v1 = new VectorText("a a b b");
-            VectorText v2 = new VectorText("b b");
-            Assert.IsTrue(Math.Abs(VectorText.Cos(v1, v2) - Math.Sqrt(2)/2) < Math.Pow(10, -8));
+            Document v1 = new Document("a a b b");
+            Document v2 = new Document("b b");
+            Assert.IsTrue(Math.Abs(Operation.Cos(v1, v2) - Math.Sqrt(2) / 2) < Math.Pow(10, -8));
         }
 
         [TestMethod]
         public void Cos2()
         {
-            VectorText v1 = new VectorText("a a b b c ");
-            VectorText v2 = new VectorText("b b a c");
-            Assert.IsTrue(Math.Abs(VectorText.Cos(v1, v2) - Math.Sqrt(6) * 7 / 18) < Math.Pow(10, -8));
+            Document v1 = new Document("a a b b c ");
+            Document v2 = new Document("b b a c");
+            Assert.IsTrue(Math.Abs(Operation.Cos(v1, v2) - Math.Sqrt(6) * 7 / 18) < Math.Pow(10, -8));
         }
 
         [TestMethod]
         public void Practice()
         {
-            VectorText v1 = new VectorText("В психологии прокрастинация — это" +
+            Document v1 = new Document("В психологии прокрастинация — это" +
                 " склонность человека к постоянному откладыванию дел на потом," +
                 " даже если они важны и требуют срочного внимания. Такое поведение," +
                 " как правило, приводит к жизненным проблемам, а также к стрессу," +
                 " потере работоспособности и производительности, чувству вины," +
                 " заниженной самооценке.");
 
-            VectorText v2 = new VectorText("Прокрастинация — это вид саморазрушительного поведения," +
+            Document v2 = new Document("Прокрастинация — это вид саморазрушительного поведения," +
                 " при котором человек откладывает решение задач на последний момент." +
                 " Нередко это сопровождается чувством вины и даже своеобразного паралича" +
                 " воли. В психологических исследованиях отмечается, что прокрастинатор" +
                 " осознает иррациональность своего бездействия и негативные последствия," +
                 " к которым такое поведение может привести.");
 
-            VectorText v3 = new VectorText("Совесть — способность личности самостоятельно" +
+            Document v3 = new Document("Совесть — способность личности самостоятельно" +
                 " формулировать нравственные обязанности и реализовывать нравственный" +
                 " самоконтроль, требовать от себя их выполнения и производить оценку" +
                 " совершаемых ею поступков; одно из выражений нравственного самосознания" +
@@ -115,10 +122,8 @@ namespace VectorText
                 " — чувства вины или «угрызений совести», то есть связывает воедино" +
                 " разум и эмоции. ");
 
-            Assert.IsTrue(Math.Abs(VectorText.Cos(v1, v2)) > Math.Abs(VectorText.Cos(v1, v3)));
-            Assert.IsTrue(Math.Abs(VectorText.Cos(v1, v2)) > Math.Abs(VectorText.Cos(v2, v3)));
+            Assert.IsTrue(Math.Abs(Operation.Cos(v1, v2)) > Math.Abs(Operation.Cos(v1, v3)));
+            Assert.IsTrue(Math.Abs(Operation.Cos(v1, v2)) > Math.Abs(Operation.Cos(v2, v3)));
         }
-
-        
     }
 }
---
+++ VectorText/Document.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace DocumentLib
+{
+    public class Document
+    {
+        public Dictionary<string, double> vector { get; private set; }
+        public Document(string textOrPath, TextOrPath TOrP = TextOrPath.Text)
+        {
+            if (TextOrPath.Path == TOrP)
+                SetTextFromFile(textOrPath);
+            else if (TextOrPath.Text == TOrP)
+                SetText(textOrPath);
+        }
+
+        private void SetText(string str)
+        {
+            Regex regex = new Regex(@"\w+");
+            var matches = regex.Matches(str);
+
+            vector = new Dictionary<string, double>();
+            foreach (Match match in matches)
+            {
+                if (!vector.ContainsKey(match.Value))
+                    vector[match.Value] = 1;
+                else
+                    vector[match.Value]++;
+            }
+
+            double norm = NormVector(vector);
+            foreach (string key in vector.Keys.ToList())
+                vector[key] /= norm;
+        }
+        private void SetTextFromFile(string path)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line = sr.ReadToEnd().ToLower();
+                SetText(line);
+            }
+        }
+
+        public static double NormVector(Dictionary<string, double> vector)
+        {
+            double sum = 0;
+            foreach (string key in vector.Keys)
+                sum += Math.Pow(vector[key], 2);
+
+            return Math.Sqrt(sum);
+        }
+    }
+    public enum TextOrPath : int
+    {
+        Text = 0,
+        Path,
+    }
+}
